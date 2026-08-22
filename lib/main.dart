61b84c29e84d6efcd2f433218b393eaaa1dbfe09
Salpicadero <juangonzalezgar194@gmail.com>
2026-08-22 09:44:51 -0500

    Inicializar formato de fecha en español

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:aldia/theme/app_theme.dart';
 import 'package:aldia/screens/login_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es_CO', null);
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
